93234bea26c7d621acdbdbd5eecadd2f887163b4
Bryan Sharpley <motosharpley@gmail.com>
2022-10-04 00:07:46 -0700

    just scribbling some notes

--- dao/src/dao.rs
+++ dao/src/dao.rs
@@ -6,6 +6,16 @@ blueprint! {
 
  impl DAO {
    pub fn instantiate_dao() -> ComponentAddress {
+    // define initial DAO owned assets
+
+    // define initial dao ownership structure
+
+    // define initial operators roles & compensation
+    // % of Dao Earnings
+
+    // define member earnings structure
+
+    // define voting levels ie common_vote, delgate_vote, founders_vote, etc.
     
      Self {}
      .instantiate()
