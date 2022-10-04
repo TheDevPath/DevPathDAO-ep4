bfe4de93066132f31412ab97b021b04c97b0eebd
Bryan Sharpley <motosharpley@gmail.com>
2022-10-03 23:03:15 -0700

    begin stub out of assets and methods

--- dao/src/accounting.rs
+++ dao/src/accounting.rs
@@ -2,13 +2,36 @@ use scrypto::prelude::*;
 
 blueprint! { 
  struct Accounting {
+  accounts_payable: Vault,
+  earnings: Vault,
+  doa_owned_member_tokens: Vault,
  }
 
  impl Accounting {
    pub fn instantiate_accounting() -> ComponentAddress {
-     Self {}
+     Self {
+      accounts_payable: Vault::new(RADIX_TOKEN),
+      earnings: Vault::new(RADIX_TOKEN),
+      doa_owned_member_tokens: Vault::new(RADIX_TOKEN),
+     }
      .instantiate()
      .globalize()
+   }
+
+   pub fn distribute_earnings() {
+    // Validate accounting_badge && founders_badge
+
+    // distribute earnings to members
+
+    // transfer doa owned earnings to accounts_payable vault
+    
+   }
+
+   pub fn pay_expenses() {
+    // Validate accounting_badge
+
+    // send expense transactions
+
    }
  }
 }
