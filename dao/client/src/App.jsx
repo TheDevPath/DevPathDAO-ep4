ca1c5cf9215bd57398e37fd8a2d8a097c5b8eb91
Bryan Sharpley <motosharpley@gmail.com>
2022-10-04 20:57:14 -0700

    init radix sdks fetch accountAddress

--- dao/client/src/App.jsx
+++ dao/client/src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import Sdk, { ManifestBuilder } from '@radixdlt/alphanet-walletextension-sdk';
+import {
+  StateApi,
+  TransactionApi,
+  StatusApi,
+} from '@radixdlt/alphanet-gateway-api-v0-sdk';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [account, setAccount] = useState('');
+  // Initialize the SDK
+  const sdk = Sdk();
+  const transactionApi = new TransactionApi();
+  const stateApi = new StateApi();
+  const statusApi = new StatusApi();
+
+  const getAddress = async () => {
+    const result = await sdk.request({
+      accountAddresses: {},
+    });
+    console.log(result.value);
+    const { accountAddresses } = result.value;
+    setAccount(accountAddresses[0].address);
+  };
+  getAddress();
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Welcome To Dev Path DAO</h2>
+      <p>Account Address: {account}</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
