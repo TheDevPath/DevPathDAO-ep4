0af8d3f3b5e389aaaa5a0048883be30245bc70bc
Bryan Sharpley <motosharpley@gmail.com>
2022-10-06 14:14:49 -0700

    stub out dashboard page

---
+++ dao/client/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Manage your Project</h2>
+      <p>Create a Proposal</p>
+      <p>Accounting</p>
+      <p>View Reports</p>
+      <p>Vote on Proposals</p>
+    </div>
+  );
+};
+
+export default Dashboard;
