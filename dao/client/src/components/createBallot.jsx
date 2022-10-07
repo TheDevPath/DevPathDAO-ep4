56b17482c9efc74a86c556ce5f277db4de8c9870
Bryan Sharpley <motosharpley@gmail.com>
2022-10-06 21:41:35 -0700

    fix formatting

--- dao/client/src/components/createBallot.jsx
+++ dao/client/src/components/createBallot.jsx
@@ -7,14 +7,14 @@ import {
   StatusApi,
 } from '@radixdlt/alphanet-gateway-api-v0-sdk';
 
-const createBallot = () => {
+const CreateBallot = () => {
   return (
     <div className="">
-      <h2>createBallot</h2>
+      <h2>CreateBallot</h2>
       <p>Add Ballot Item</p>
       <button>Create</button>
     </div>
   );
 };
 
-export default createBallot;
+export default CreateBallot;
