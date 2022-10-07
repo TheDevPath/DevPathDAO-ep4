7eba55a832487af3cb871f980d0f2185f1770aa8
Bryan Sharpley <motosharpley@gmail.com>
2022-10-06 21:42:05 -0700

    stub out proposals page

---
+++ dao/client/src/pages/Proposals.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Proposals = () => {
+  return (
+    <div>
+      <h2>Review and Vote on Proposals</h2>
+      <p>Currently Active</p>
+      <p>Historical</p>
+    </div>
+  );
+};
+
+export default Proposals;
