7aa885b4ceb124ed8d80c7362c1060068cf67c43
Bryan Sharpley <motosharpley@gmail.com>
2022-10-06 21:41:51 -0700

    stub out landing page

---
+++ dao/client/src/pages/Lander.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Lander = () => {
+  return (
+    <div>
+      <h1>Welcome to Dev Path DAO</h1>
+      <p>Community Drive Development</p>
+      <p>Incubating the next generation of Entreprenuers.</p>
+    </div>
+  );
+};
+
+export default Lander;
