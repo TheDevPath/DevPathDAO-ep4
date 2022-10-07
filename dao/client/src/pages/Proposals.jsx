40d01d78af812ced657de6d3bc02cd0ca0f13af3
Bryan Sharpley <motosharpley@gmail.com>
2022-10-06 21:58:21 -0700

    housekeeping

--- dao/client/src/pages/Proposals.jsx
+++ dao/client/src/pages/Proposals.jsx
@@ -1,4 +1,11 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+// Import Radix Wallet and Gateway SDKs
+import Sdk, { ManifestBuilder } from '@radixdlt/alphanet-walletextension-sdk';
+import {
+  StateApi,
+  TransactionApi,
+  StatusApi,
+} from '@radixdlt/alphanet-gateway-api-v0-sdk';
 
 const Proposals = () => {
   return (
