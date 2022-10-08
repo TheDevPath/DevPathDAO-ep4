2213a43293326734033f52ac87c3b34760ed1c48
Bryan Sharpley <motosharpley@gmail.com>
2022-10-07 21:38:27 -0700

    stub out marketplace page

---
+++ dao/client/src/pages/Marketplace.jsx
@@ -0,0 +1,14 @@
+import BuyTokens from '../components/BuyTokens';
+
+const Marketplace = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-2">
+        Welcome To The DAO Marketplace
+      </h2>
+      <BuyTokens />
+    </div>
+  );
+};
+
+export default Marketplace;
