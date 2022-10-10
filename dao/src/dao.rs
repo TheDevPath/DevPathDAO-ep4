0bb8024218627725cc83d476ee73301071493067
Bryan Sharpley <motosharpley@gmail.com>
2022-10-09 19:33:53 -0700

    setting up structure for DAO creation process

--- dao/src/dao.rs
+++ dao/src/dao.rs
@@ -1,12 +1,29 @@
 use scrypto::prelude::*;
 
+#[derive(NonFungibleData)]
+pub struct DAO {
+  pub name: String,
+  pub desc: String,
+  pub num_shares: u32,
+}
+
 blueprint! { 
  struct DAO {
+  dao_token_vault: Vault,
  }
 
  impl DAO {
-   pub fn instantiate_dao() -> ComponentAddress {
+   pub fn instantiate_dao(dao_name: String, description: String, num_shares: u32) -> ComponentAddress {
+    // possible enhancement Mint NFT defining the organization
     // define initial DAO owned assets
+    let total_shares = num_shares.to_string();
+
+    let dao_bucket: Bucket = ResourceBuilder::new_fungible()
+         .metadata("dao_name", dao_name)
+         .metadata("description", description)
+         .metadata("num_shares", total_shares)
+         .initial_supply(1);
+    
 
     // define initial dao ownership structure
 
@@ -17,7 +34,9 @@ blueprint! {
 
     // define voting levels ie common_vote, delgate_vote, founders_vote, etc.
     
-     Self {}
+     Self {
+      dao_token_vault: Vault::with_bucket(dao_bucket),
+     }
      .instantiate()
      .globalize()
    }
