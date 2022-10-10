68d902ed786d2ad7ef27d4166abb92bf9922b8f4
Bryan Sharpley <motosharpley@gmail.com>
2022-10-10 11:24:37 -0700

    add total shares input and i/o comments

--- dao/src/members.rs
+++ dao/src/members.rs
@@ -1,4 +1,7 @@
 use scrypto::prelude::*;
+// Members component is used to create DAO Badges and set up Member Token Sales
+// founders_badge is returned to instantiator @@@ THIS IS YOUR SUPER USER BADGE @@@
+// founders_badge is required to withdraw token sale proceeds and change member token price
 
 blueprint! { 
  struct Members {
@@ -8,17 +11,23 @@ blueprint! {
   operator_badge_vault: Vault,
   delegate_badge_vault: Vault,
   xrd_tokens_vault: Vault,
-  price_per_token: Decimal
+  price_per_token: Decimal,
+  total_shares: u128,
  }
 
+// @instantiate_members initializes all badges, member tokens and vaults 
+// @price_per_token sets initial member token sale price
+// @total_shares sets initial supply of member tokens with divisiblity maximum as default to allow for split shares
+// @returns single founders_badge
+
  impl Members {
-   pub fn instantiate_members(price_per_token: Decimal) -> (ComponentAddress, Bucket ){
+   pub fn instantiate_members(price_per_token: Decimal, total_shares: u128) -> (ComponentAddress, Bucket ){
 
      let member_token = ResourceBuilder::new_fungible()
      .divisibility(DIVISIBILITY_MAXIMUM)
      .metadata("name", "Member Token")
      .metadata("symbol", "MEMBER")
-     .initial_supply(1000);
+     .initial_supply(total_shares);
    
      let founders_badge: Bucket = ResourceBuilder::new_fungible()
           .divisibility(DIVISIBILITY_NONE)
@@ -57,7 +66,8 @@ blueprint! {
           accounting_badge_vault: Vault::with_bucket(accounting_badge),
           operator_badge_vault: Vault::with_bucket(operator_badge),
           delegate_badge_vault: Vault::with_bucket(delegate_badge),
-          price_per_token: price_per_token
+          price_per_token: price_per_token,
+          total_shares: total_shares,
      }
      .instantiate();
      component.add_access_check(access_rules);
